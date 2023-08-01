ad589327ac6af62fc1b8aeae90ba0d841895e501
Nitroceliulioze <aksionova.diana@gmail.com>
2023-08-01 10:29:56 +0300

    add tasks, task and deleteTask

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,14 @@
 import PropTypes from 'prop-types'
-
+import Button from './Button'
 
 const Header = ({title}) => {
+  const onClick = () => {
+    console.log('click from header')
+  }
   return (
-    <header>
-      <h1>{title}</h1>
+    <header className='header'>
+      <h1 >{title}</h1>
+      <Button color='green' text='Add' onClick={onClick}/>
     </header>
   )
 }
@@ -15,5 +19,12 @@ Header.defaultProps = {
 Header.propTypes = { 
     title: PropTypes.string.isRequired
 }
+
+// CSS in JS
+// const headingStyles ={
+//   color: 'red',
+//   background: "black"
+
+// }
 
 export default Header
